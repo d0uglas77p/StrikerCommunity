88dd7dfe1faccd6d4c4fd02addf39a7875ef4710
d0uglas77p <douglas_1313@hotmail.com>
2024-06-30 22:19:56 -0300

    add menu/paginas

--- src/main/java/application/bens/NavegacaoBean.java
+++ src/main/java/application/bens/NavegacaoBean.java
@@ -1,7 +1,7 @@
 package application.bens;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
 
 import java.io.Serializable;
 
@@ -15,24 +15,28 @@ public class NavegacaoBean implements Serializable{
         return "/inicio.xhtml?faces-redirect=true";
     }
 
-    public String irCadastro() {
-        return "/cadastro.xhtml?faces-redirect=true";
-    }
-
     public String irLogin() {
         return "/login.xhtml?faces-redirect=true";
     }
 
-    public String irTutorial() {
-        return "/tutorial.xhtml?faces-redirect=true";
+    public String irAprenda() {
+        return "/aprenda.xhtml?faces-redirect=true";
+    }
+
+    public String irForum() {
+        return "/forum.xhtml?faces-redirect=true";
+    }
+
+    public String irRanking() {
+        return "/ranking.xhtml?faces-redirect=true";
+    }
+
+    public String irVip() {
+        return "/vip.xhtml?faces-redirect=true";
     }
 
     public String irMeuPerfil() {
         return "/meuperfil.xhtml?faces-redirect=true";
-    }
-
-    public String irTeste() {
-        return "/teste.xhtml?faces-redirect=true";
     }
 
 
