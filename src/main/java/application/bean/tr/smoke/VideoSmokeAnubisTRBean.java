9aee465df59bab9ebf609e2372e55bd24d2fca21
d0uglas77p <douglas_1313@hotmail.com>
2024-07-07 23:11:26 -0300

    beans smokes tr mapas

--- src/main/java/application/bean/tr/smoke/VideoSmokeAnubisTRBean.java
+++ src/main/java/application/bean/tr/smoke/VideoSmokeAnubisTRBean.java
@@ -1,4 +1,62 @@
 package application.bean.tr.smoke;
 
-public class VideoSmokeAnubisTRBean {
+import application.model.Video;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.io.InputStream;
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@ManagedBean
+@ViewScoped
+public class VideoSmokeAnubisTRBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(VideoSmokeAnubisTRBean.class.getName());
+    private List<Video> videosSmokeAnubis;
+
+    @PostConstruct
+    public void init() {
+        videosSmokeAnubis = new ArrayList<>();
+        Properties props = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            inputStream = context.getExternalContext().getResourceAsStream("/resources/properties/videos/terrorista/anubis/smoke.properties");
+            if (inputStream != null) {
+                props.load(inputStream);
+                int i = 1;
+                while (props.containsKey("video" + i + ".titulo")) {
+                    String titulo = props.getProperty("video" + i + ".titulo");
+                    String url = props.getProperty("video" + i + ".url");
+                    videosSmokeAnubis.add(new Video(titulo, url));
+                    i++;
+                }
+            } else {
+                throw new IOException("Arquivo smoke.properties não encontrado no diretório /resources");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<Video> getVideosSmokeAnubis() {
+        LOGGER.info("Exibindo painel de Smokes Anubis");
+        return videosSmokeAnubis;
+    }
 }
--- src/main/java/application/bean/tr/smoke/VideoSmokeDustTRBean.java
+++ src/main/java/application/bean/tr/smoke/VideoSmokeDustTRBean.java
@@ -1,6 +1,5 @@
 package application.bean.tr.smoke;
 
-import application.bean.UtilitarioPanelBean;
 import application.model.Video;
 
 import javax.annotation.PostConstruct;
@@ -15,17 +14,16 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-
 @ManagedBean
 @ViewScoped
 public class VideoSmokeDustTRBean implements Serializable {
     private static final long serialVersionUID = 1L;
-    private static final Logger LOGGER = Logger.getLogger(UtilitarioPanelBean.class.getName());
-    private List<Video> videos;
+    private static final Logger LOGGER = Logger.getLogger(VideoSmokeDustTRBean.class.getName());
+    private List<Video> videosSmokeDust;
 
     @PostConstruct
     public void init() {
-        videos = new ArrayList<>();
+        videosSmokeDust = new ArrayList<>();
         Properties props = new Properties();
         InputStream inputStream = null;
 
@@ -38,7 +36,7 @@ public class VideoSmokeDustTRBean implements Serializable {
                 while (props.containsKey("video" + i + ".titulo")) {
                     String titulo = props.getProperty("video" + i + ".titulo");
                     String url = props.getProperty("video" + i + ".url");
-                    videos.add(new Video(titulo, url));
+                    videosSmokeDust.add(new Video(titulo, url));
                     i++;
                 }
             } else {
@@ -57,9 +55,9 @@ public class VideoSmokeDustTRBean implements Serializable {
         }
     }
 
-    public List<Video> getVideos() {
+    public List<Video> getVideosSmokeDust() {
         LOGGER.info("Exibindo painel de Smokes Dust");
-        return videos;
+        return videosSmokeDust;
     }
 
 }
