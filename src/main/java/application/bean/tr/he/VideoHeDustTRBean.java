0e416f3f4d6a389232214f2eb5fa3a3cf41fe3a6
d0uglas77p <douglas_1313@hotmail.com>
2024-07-08 17:51:32 -0300

    he panel/beans

--- src/main/java/application/bean/tr/he/VideoHeDustTRBean.java
+++ src/main/java/application/bean/tr/he/VideoHeDustTRBean.java
@@ -1,4 +1,62 @@
 package application.bean.tr.he;
 
-public class VideoHeDustTRBean {
+import application.model.Video;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.io.InputStream;
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@ManagedBean
+@ViewScoped
+public class VideoHeDustTRBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(VideoHeDustTRBean.class.getName());
+    private List<Video> videosHeDust;
+
+    @PostConstruct
+    public void init() {
+        videosHeDust = new ArrayList<>();
+        Properties props = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            inputStream = context.getExternalContext().getResourceAsStream("/resources/properties/videos/terrorista/dust/he.properties");
+            if (inputStream != null) {
+                props.load(inputStream);
+                int i = 1;
+                while (props.containsKey("video" + i + ".titulo")) {
+                    String titulo = props.getProperty("video" + i + ".titulo");
+                    String url = props.getProperty("video" + i + ".url");
+                    videosHeDust.add(new Video(titulo, url));
+                    i++;
+                }
+            } else {
+                throw new IOException("Arquivo smoke.properties não encontrado no diretório /resources");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<Video> getVideosHeDust() {
+        LOGGER.info("Exibindo painel de HE Dust");
+        return videosHeDust;
+    }
 }
--- src/main/java/application/bean/tr/he/VideoHeVertigoTRBean.java
+++ src/main/java/application/bean/tr/he/VideoHeVertigoTRBean.java
@@ -1,4 +1,62 @@
 package application.bean.tr.he;
 
-public class VideoHeVertigoTRBean {
+import application.model.Video;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@ManagedBean
+@ViewScoped
+public class VideoHeVertigoTRBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(VideoHeVertigoTRBean.class.getName());
+    private List<Video> videosHeVertigo;
+
+    @PostConstruct
+    public void init() {
+        videosHeVertigo = new ArrayList<>();
+        Properties props = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            inputStream = context.getExternalContext().getResourceAsStream("/resources/properties/videos/terrorista/vertigo/he.properties");
+            if (inputStream != null) {
+                props.load(inputStream);
+                int i = 1;
+                while (props.containsKey("video" + i + ".titulo")) {
+                    String titulo = props.getProperty("video" + i + ".titulo");
+                    String url = props.getProperty("video" + i + ".url");
+                    videosHeVertigo.add(new Video(titulo, url));
+                    i++;
+                }
+            } else {
+                throw new IOException("Arquivo smoke.properties não encontrado no diretório /resources");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<Video> getVideosHeVertigo() {
+        LOGGER.info("Exibindo painel de HE Vertigo");
+        return videosHeVertigo;
+    }
 }
