b4a855dcd695810bcd857f4cf8da834b7606e1cc
d0uglas77p <douglas_1313@hotmail.com>
2024-07-08 17:27:48 -0300

    flash panel/beans

--- src/main/java/application/bean/tr/flash/VideoFlashAncientTRBean.java
+++ src/main/java/application/bean/tr/flash/VideoFlashAncientTRBean.java
@@ -1,4 +1,62 @@
 package application.bean.tr.flash;
 
-public class VideoFlashAncientTRBean {
+import application.model.Video;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.io.InputStream;
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@ManagedBean
+@ViewScoped
+public class VideoFlashAncientTRBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(VideoFlashAncientTRBean.class.getName());
+    private List<Video> videosFlashAncient;
+
+    @PostConstruct
+    public void init() {
+        videosFlashAncient = new ArrayList<>();
+        Properties props = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            inputStream = context.getExternalContext().getResourceAsStream("/resources/properties/videos/terrorista/ancient/flash.properties");
+            if (inputStream != null) {
+                props.load(inputStream);
+                int i = 1;
+                while (props.containsKey("video" + i + ".titulo")) {
+                    String titulo = props.getProperty("video" + i + ".titulo");
+                    String url = props.getProperty("video" + i + ".url");
+                    videosFlashAncient.add(new Video(titulo, url));
+                    i++;
+                }
+            } else {
+                throw new IOException("Arquivo smoke.properties não encontrado no diretório /resources");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<Video> getVideosFlashAncient() {
+        LOGGER.info("Exibindo painel de Flash Ancient");
+        return videosFlashAncient;
+    }
 }
--- src/main/java/application/bean/tr/flash/VideoFlashOverpassTRBean.java
+++ src/main/java/application/bean/tr/flash/VideoFlashOverpassTRBean.java
@@ -1,4 +1,62 @@
 package application.bean.tr.flash;
 
-public class VideoFlashOverpassTRBean {
+import application.model.Video;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@ManagedBean
+@ViewScoped
+public class VideoFlashOverpassTRBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(VideoFlashOverpassTRBean.class.getName());
+    private List<Video> videosFlashOverpass;
+
+    @PostConstruct
+    public void init() {
+        videosFlashOverpass = new ArrayList<>();
+        Properties props = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            inputStream = context.getExternalContext().getResourceAsStream("/resources/properties/videos/terrorista/overpass/flash.properties");
+            if (inputStream != null) {
+                props.load(inputStream);
+                int i = 1;
+                while (props.containsKey("video" + i + ".titulo")) {
+                    String titulo = props.getProperty("video" + i + ".titulo");
+                    String url = props.getProperty("video" + i + ".url");
+                    videosFlashOverpass.add(new Video(titulo, url));
+                    i++;
+                }
+            } else {
+                throw new IOException("Arquivo smoke.properties não encontrado no diretório /resources");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<Video> getVideosFlashOverpass() {
+        LOGGER.info("Exibindo painel de Flash Overpass");
+        return videosFlashOverpass;
+    }
 }
